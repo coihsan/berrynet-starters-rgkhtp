f47c76bd7d9fb15284ac639c604917a0200500dc
coihsan <ihsan.onlinefs@gmail.com>
2023-10-28 00:52:07 +0700

    commit

--- components/navigation/navbar.tsx
+++ components/navigation/navbar.tsx
@@ -1,4 +1,4 @@
-
+import Link from 'next/link'
 const navitems = [
     {
         title: "Home",
@@ -33,12 +33,15 @@ const navitems = [
 
 export default function Navbar(){
     return (
-        <nav>
-            <div className="max-w-screen-xl fixed fixed left-0 top-0 right-0 w-full border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-                <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
+        <nav className="fixed left-0 top-0 right-0 w-full border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
+            <div className="max-w-screen-xl mx-auto ">
+                <div className="z-10 items-center justify-between font-mono text-sm lg:flex">
+                    <Link href='#' className="font-bold">Berrynet</Link>
+                    <div className='flex items-center bg-rose-400 px-3 py-1'>
                     {navitems.map((menu) =>(
-                        
+                        <Link href={menu.path} >{menu.title}</Link>
                     ))}
+                    </div>
                 </div>
             </div>
         </nav>

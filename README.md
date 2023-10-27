bb0eaad57e4b3612a5222f5d2c8a129d2a03fbdc
coihsan <ihsan.onlinefs@gmail.com>
2023-10-27 16:33:06 +0700

    commit

--- README.md
+++
@@ -1,3 +0,0 @@
-# berrynet-starters-rgkhtp
-
-[Edit in Codeflow ⚡️](https://stackblitz.com/~/github.com/coihsan/berrynet-starters-rgkhtp)
--- components/hero.tsx
+++ components/hero.tsx
@@ -25,7 +25,7 @@ const SERVICES_DATA = [
 ]
 export function Segmentation(){
     return (
-        <div className='grid grid-cols-3 max-[600px]:grid-cols-1 gap-5 max-w-screen-xl mx-atuo '>
+        <div className='grid grid-cols-3 max-[600px]:grid-cols-1 gap-5 w-full max-w-screen-xl mx-atuo '>
             {SERVICES_DATA.map((data) =>(
                 <div className='rounded-xl group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30'>
                     <Link
@@ -50,7 +50,7 @@ export function Segmentation(){
 
 const Hero = () =>{
     return (
-        <section className='pt-24 px-2 max-w-screen-xl mx-atuo bg-none h-[100vh]'>
+        <section className='px-2 flexColCenter max-w-screen-lg w-full mx-atuo bg-none h-[100vh]'>
             <center className='text-7xl max-[600px]:text-4xl font-medium'>Experience the Difference of High-Speed Internet</center>
             <center>Get the fastest and most reliable internet speeds in your area, with plans to fit every budget.</center>
             <Segmentation />

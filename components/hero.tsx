76713e8a07c034c32d97fa3b17529ea965ce5f4f
coihsan <ihsan.onlinefs@gmail.com>
2023-10-28 15:23:16 +0700

    commit

--- components/hero.tsx
+++ components/hero.tsx
@@ -4,40 +4,41 @@ const SERVICES_DATA = [
     {
         id: '01',
         title: 'Home Internet',
-        desc: 'Lorem ipsum dolor sit amet',
+        desc: "Get fast and unlimited internet service with attractive service packages.",
         path: '/Homenet',
-        icon: '#'
+        icon: '#',
     },
     {
         id: '02',
         title: 'Apartment Internet',
-        desc: 'Lorem ipsum dolor sit amet',
+        desc: "We are also available for internet services in many Apartments that have partnered with us.",
         path: '/Apartmentnet',
         icon: '#'
     },
     {
         id: '03',
         title: 'Business Internet',
-        desc: 'Lorem ipsum dolor sit amet',
+        desc: "Do you need dedicated internet service or just broadband with mega-bandwith? We have it!",
         path: '/Businessnet',
         icon: '#'
     }
 ]
 export function Segmentation(){
     return (
-        <div className='grid grid-cols-3 max-[600px]:grid-cols-1 gap-5 w-full max-w-screen-xl mx-atuo '>
+        <div className='grid grid-cols-3 max-[600px]:grid-cols-1 gap-5 max-w-screen-xl'>
             {SERVICES_DATA.map((data) =>(
-                <div className='rounded-xl group rounded-lg border border-transparent px-5 py-4 transition-all hover:translate-y-[-5px] hover:border-gray-300 ease-in-out hover:bg-indigo-100 hover:dark:border-indigo-700 hover:dark:bg-indigo-800/30'>
+                <div className={'group rounded-lg w-full transition-all hover:translate-y-[-5px] ease-in-out bg-indigo-600 px-5 py-10'}>
                     <Link
                     href={data.path}
-                    className=""
+                    className="flexColCenter gap-3"
                     rel="noopener noreferrer"
                     >
-                    <h2 className={`mb-3 text-2xl font-bold`}>{data.title}</h2>
-                    <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-                        {data.desc}
+                    <h2 className={`text-4xl font-medium`}>{data.title}</h2>
+                    <p className={`m-0 text-sm text-center`}>{data.desc}</p>
+                    <div className='group-hover:bg-white text-indigo-700 bg-white py-2 px-5 transition-all rounded-full flexCenter'>
+                        <p className="">Visit</p>
                         <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">-&gt;</span>
-                    </p>
+                    </div>
                     </Link>
                 </div>
             ))}
@@ -47,11 +48,11 @@ export function Segmentation(){
 
 const Hero = () =>{
     return (
-        <section className='px-2 flexColCenter gap-5 w-full mx-atuo h-[100vh]'>
-            <center className='text-7xl max-w-screen-lg max-[600px]:text-4xl font-medium'>Experience the Difference of High-Speed Internet</center>
-            <center>Get the fastest and most reliable internet speeds in your area, with plans to fit every budget.</center>
+        <section className='px-2 flexColCenter gap-5 w-full h-[100vh]'>
+            <center className='text-7xl max-w-screen-lg max-[600px]:text-4xl font-bold text-white '>Experience the Difference of High-Speed Internet</center>
+            <center className='text-neutral-500'>Get the fastest and most reliable internet speeds in your area, with plans to fit every budget.</center>
             <div className='pt-10 flexColCenter gap-3'>
-                <center>What type of internet do you need?</center>
+                <center className='mb-5 text-[1.5rem]'>What type of internet do you need?</center>
                 <Segmentation />
             </div>
         </section>
--- components/navigation/navbar.tsx
+++ components/navigation/navbar.tsx
@@ -31,14 +31,14 @@ const navitems = [
 
 export default function Navbar(){
     return (
-        <nav className="fixed w-[100%] bg-indigo-700 dark:bg-indigo-800 h-auto">
+        <nav className="fixed w-[100%] backdrop-blur-xl bg-white/10 h-auto">
             <div className="lg:p-4 items-center justify-between text-sm lg:flex  max-w-screen-xl mx-auto">
                     <Link href='/' className="font-bold hover:opacity-[0.7]">
                         <Image src='/berrynet.svg' alt='Logo' width={200} height={60} priority/>
                     </Link>
                     <div className='flex items-center'>
                     {navitems.map((menu) =>(
-                        <Link href={menu.path} className='text-white hover:text-indigo-600 px-5 py-2 hover:bg-indigo-300 rounded-full transition-all'> {menu.title}</Link>
+                        <Link href={menu.path} className='text-white hover:text-indigo-100 px-5 py-2 hover:bg-indigo-600 rounded-full transition-all'> {menu.title}</Link>
                     ))}
                     <DarkMode />
                     </div>

f2a8c2724246aefccb7d1b4f363a232d43697b9f
coihsan <ihsan.onlinefs@gmail.com>
2023-11-01 22:02:28 +0700

    commit

--- app/coverage/page.tsx
+++ app/coverage/page.tsx
@@ -4,17 +4,16 @@ export default function Coverage(){
             <h1 className="text-3xl text-center pb-[2rem]">Find berrynet in your area</h1>
                     <div className='flex flex-col gap-3 max-w-3xl mx-auto bg-rose-800'>
                         <select className='text-black bg-white max-w-md rounded-md h-[2rem]' name="" id="" placeholder='Select area'>
-                            <option value="01" disabled>Select Area :</option>
-                            <option value="01">Jakarta Barat</option>
-                            <option value="Jakarta">Jakarta Utara</option>
-                            <option value="Banten">Tangerang Kota</option>
-                            <option value="Banten">Tangerang Selatan</option>
-                            <option value="Bogor">Bogor</option>
-                            <option value="Bekasi">Bekasi</option>
-                            <option value="Surabaya">Surabaya</option>
-                            <option value="Bandung">Bandung</option>
+                            <option value="" disabled>Select Area :</option>
+                            <option value="jakartabarat">Jakarta Barat</option>
+                            <option value="jakartautara">Jakarta Utara</option>
+                            <option value="tangerangkota">Tangerang Kota</option>
+                            <option value="tangerangselatan">Tangerang Selatan</option>
+                            <option value="bogor">Bogor</option>
+                            <option value="bekasi">Bekasi</option>
+                            <option value="surabaya">Surabaya</option>
+                            <option value="bandung">Bandung</option>
                         </select>
-                        
                     </div>
         </section>
     )

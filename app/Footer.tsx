d2ae4c56a8acb3e21ef8c7605d29b656fa9cfc66
coihsan <ihsan.onlinefs@gmail.com>
2023-11-01 10:10:18 +0700

    commit

--- app/Footer.tsx
+++ app/Footer.tsx
@@ -1,12 +1,18 @@
+'use client'
+import React, {useState} from 'react'
 import {quicklink, ourproducts, company, helpsupport} from '@/app/components/constants/data'
 import Link from 'next/link'
 import Image from 'next/image'
 
 export default function Footer(){
+    const [isExpanded, setIsExpanded] = useState(false);
+
+    const buttonClassName = `${isExpanded ? "hidden" : "block"} text-indigo-500 text-[1.2rem]`;
+
     return(
         <footer className='containers-center'>
             
-            <div className='flex justtify-between gap-10 py-20'>
+            <div className='flex justtify-between gap-10 py-20 max-[600px]:flex-col'>
                 <div className=''>
                     <div className='max-w-lg flex flex-col gap-3 '>
                         <Image
@@ -20,12 +26,15 @@ export default function Footer(){
                         <p className='text-[14px]'>Our company is committed to providing the best possible internet service to our customers. We offer a variety of plans to fit every budget and need, and we guarantee fast, reliable connections.</p>
                     </div>
                 </div>
-                <div className='grid grid-cols-4 gap-5  w-full'>
+                <div className='grid grid-cols-4 gap-5 w-full max-[600px]:grid-cols-1'>
                 <div>
-                    <h3 className='text-indigo-500 text-[1.2rem]'>Quick Links</h3>
-                    <ul className='flex flex-col gap-2 pt-[1rem] '>
+                    <button type='button' 
+                        onClick={() => setIsExpanded(!isExpanded)}
+                        className={'buttonClassName text-indigo-500 text-[1.2rem]'}>Quick Links
+                    </button>
+                    <ul className={'max flex flex-col gap-2 pt-[1rem] '}>
                         {quicklink.map((quicklink)=>(
-                            <li className='hover:translate-x-[5px] text-[14px] transition-transform ease-in-out cursor-pointer'><Link className='' href={quicklink.path}>{quicklink.title}</Link></li>
+                            <li className={'${isExpanded ? "block" : "hidden"}  hover:translate-x-[5px] text-[14px] transition-transform ease-in-out cursor-pointer'}><Link className='' href={quicklink.path}>{quicklink.title}</Link></li>
                         ))}
                     </ul>
                 </div>
